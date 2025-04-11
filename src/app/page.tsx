9bd089b214f30b96f748364022aed0677e7a12ed
Abheeshta-P <softwarecheck01+github@gmail.com>
2025-04-11 21:12:04 +0530

    fix button component and hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="w-full min-h-screen flex items-center flex-col justify-center">
-      <h1 className="font-extrabold text-6xl">Social media analytics</h1>
-      <p className="text-gray-400 font-semibold mt-3 text-lg">Navigate the digital world with ease and smart moves.</p>
-      <div className="flex gap-12 mt-10">
+      <h1 className="font-extrabold text-3xl sm:text-4xl md:text-5xl lg:text-6xl">Social media analytics</h1>
+      <p className="text-gray-400 font-semibold mt-3 text-sm sm:text-base md:text-lg">Navigate the digital world with ease and smart moves.</p>
+      <div className="flex gap-4 md:gap-6 lg:gap-12 mt-10">
         <Link href={"top-users"}><Button>Check out Top Users</Button></Link>
         <Link href={"trending-posts"}><Button bg={"bg-gray-50"} color={"text-gray-900"} className={"hover:bg-zinc-900 hover:text-white"}>Grab the Trending Posts</Button></Link>
         <Link href={"feed"}><Button>Go to Feed</Button></Link>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react'
 import classNames from 'classnames'
 
-type Size = "small" | "medium" | "large";
+type Size = "small" | "medium" | "large" | "responsive";
 
 type ButtonProps = {
   children: ReactNode,
@@ -12,19 +12,24 @@ type ButtonProps = {
 }
 
 const sizeClasses: Record<Size, string> = {
-  "small": "px-2 py-2 text-xs max-w-36",
-  "medium": "px-4 py-2.5 text-sm",
-  "large": "px-6 py-3 text-base"
-}
+  small: "px-2 py-2 text-xs max-w-36",
+  medium: "px-4 py-2.5 text-sm",
+  large: "px-6 py-3 text-base",
+  responsive: `
+    px-2 py-2 text-xs
+    sm:px-4 sm:py-2.5 sm:text-sm
+    md:px-6 md:py-3 md:text-base
+  `
+};
 
 function Button({
   children,
   bg = "bg-black",
   color = "text-white",
   className = "",
-  size = "large",
+  size = "responsive",
   ...rest
-}:ButtonProps) {
+}: ButtonProps) {
   return (
     <div
       {...rest}
